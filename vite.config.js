b50112909b3009884ac66693ba8731af7cc3a092
ryu1uga <20211953@aloe.ulima.edu.pe>
2026-05-26 17:06:33 -0500

    Revert "allow hosts"
    This reverts commit 930f975061e194a6430326bc3fc1b13f4e80ca56.

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    base: './',
     plugins: [react()],
     server: {
         port: 3000,
-<<<<<<< HEAD
         host: true,
         allowedHosts: true
-    },
-    build: {
-        target: 'es2015'
-=======
-        host: true
->>>>>>> parent of 930f975 (allow hosts)
     }
 })
